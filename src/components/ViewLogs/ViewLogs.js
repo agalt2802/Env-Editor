d80570d15e8622400baf99aff0a371d28322a3c8
Francesco Magoga <f.magoga@reply.it>
2023-08-22 15:57:44 +0200

    Changed logs UI pt.2

--- src/components/ViewLogs/ViewLogs.js
+++ src/components/ViewLogs/ViewLogs.js
@@ -1,96 +1,40 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Label, Input } from "reactstrap";
-import { Modal, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Label, Input, FormGroup } from "reactstrap";
 
 import Logs from "./Logs";
-import { fetchWithCatch } from "../../commonFunctions";
 
 function ViewLogs() {
-  const [selectedDate, setSelectedDate] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [logs, setLogs] = useState([]);
+  const today = new Date();
 
-  let today = new Date().toISOString().split("T")[0];
-  let date;
-  console.log(today);
+  const [date, setDate] = useState(today);
 
-  const handleSelectDate = (event) => {
-    setSelectedDate(date);
-    if (date > today) {
-      setLogs(false)
-      setShowModal(true);
-    } else {
-      fetchWithCatch("/getLogs", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ date: date }),
-      }, (json) => {
-        setLogs(json);
-        console.log("LOGS: " + JSON.stringify(logs));  
+  const handlePickDate = (event) =>
+  {
+    let date = new Date(event.target.value);
 
-        if(!json)
-        {
-          setShowModal(true);
-          setSelectedDate(date);
-        }
-      }, (e) => {}, true);
-    }
-  };
-
-  const handlePickDate = (event) =>{
-    console.log(event.target)
-    date = new Date(event.target.value);
-    date = date.toISOString().split("T")[0];
+    setDate(date);
   }
 
-  const handleModel = () => {
-    setShowModal(false);
-    // setSelectedDate("");
-  };
-
   return (
-    <Container id="viewLogsContainer">
-      <h1>View Logs</h1>
-      <Label>Select a day</Label>
+    <Container id="mainContainer">
       <Row>
         <Col>
-      <Input
-        type="date"
-        value={date}
-        onChange={handlePickDate}
-      ></Input>
-      </Col>
-      <Col>
-      <Button onClick={handleSelectDate}>Select Date</Button>
-      </Col>
+          <h1>View Logs</h1>
+        </Col>
       </Row>
       <Row>
-        { logs &&
-           <Logs logs={logs} setLogs={setLogs} selectedDate={selectedDate} />
-        }
-     
+        <Col>
+          <FormGroup>
+            <Label>Logs date</Label>
+            <Input
+              type="date"
+              value={date.toISOString().substring(0,10)}
+              onChange={handlePickDate}
+            />
+          </FormGroup>
+        </Col>
       </Row>
-      
-      <Modal
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        onChange={(event) => event.preventDefault()}
-      >
-        <Modal.Header>Invalid Date</Modal.Header>
-        <Modal.Body>
-          {console.log("Selected Day: " + selectedDate)}
-          {selectedDate > today
-            ? "Please select a valid date!"
-            : "No logs avilable for this date"}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button color="success" onClick={handleModel}>
-            DONE
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <Logs key={date} date={date} />
     </Container>
   );
 }
